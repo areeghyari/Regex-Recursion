8143f49b6ed4d317ca33021c8ff5916faec4df26
areeghyari <areegbazbaz@gmail.com>
2022-02-10 10:25:53 +0200

    Last

--- regex.test.js
+++ regex.test.js
@@ -5,53 +5,34 @@
 /* Write a function that take a string and return true if the string only contain uppercase and lowercase
 characters (no numbers and symbols) and it should end with capital A else return false */
 
-function capitalA(s){
-    function capitalA(s) {
-        let regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(A$)/g;
-    
-        if (regex.test(s)) {
-            return "true";
-        } else {
-            return "false";
-        }
-    
-    }
-    console.log(capitalA(s));
-
+function capitalA(s) {
+    let regNum =  /[A-Za-z ]*A$/;
+    let finRes = regNum.test(s);
+    return finRes;
 }
 
 
 /* Write a function that take a string and return true if the the sting is following the emails pattern
 which end with io (example@example.io) */
 
-function ioEmail(email){
-    let regex = /^(.+)@(\S+)io$./ig;
-    if (regex.test(email)) {
-        return "true";
-    } else {
-    return;
+function ioEmail(email) {
+    let regNum = /^[a-zA-Z0-9+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(example)\.io$/g;
+    let finRes = regNum.test(email);
+    return finRes;
 }
-
 /* You have a text that contain image names with their extention you need to write a function to 
 find all images in that text and return their names and extention in an array 
 required extention are jpg, jpeg and png.
 */
 
-    function imagesSearcher(imageName, text) {
-        let arr = [];
-        let regex = /jpg/;
-        let regext = /jpeg/;
-        let regext = /png/;
-    
-        if (regex.match(text) = true || (regex.match(text) = true || (regex.match(text) = true) )){
-            arr.push(imageName);
-        } else { }
-    }
-        return arr
+function imagesSearcher( result) {
+    let arr = result.match(/[\w-]+\.(jpg|png|jpeg)/g);
+    if (arr == null) return [];
+    else return arr;
 }
 
 
-describe("Test capitalA", ()=>{
+describe("Test capitalA", () => {
     test("It should return true if the input has uppercase and lowercase characters (no numbers and symbols) and it should end with capital A else return false ", () => {
         expect(capitalA("Hello world A")).toStrictEqual(true);
 
